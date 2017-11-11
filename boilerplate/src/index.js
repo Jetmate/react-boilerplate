761e1255638c0fa74be2f413ddb594555850ddcb
Jetmate <antonoutkine@gmail.com>
2017-11-11 00:19:39 -0600

    Fix for codesandbox

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import Router from './navigation'
-import 'shared/style/globals'
+import './shared/style/globals'
 
 ReactDOM.render(
   <BrowserRouter>
     <Router />
   </BrowserRouter>
-  , document.getElementById('app')
+  , document.getElementById('root')
 )
